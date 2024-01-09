a132b4a91b0a789e23f6baf4d75d7634dcafd065
Villerot Justin <justinvillerot@orange.fr>
2024-01-09 11:28:58 +0100

    Space Color changer

---
+++ Assets/Scripts/ChangeSpaceColor.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class ChangeSpaceColor : MonoBehaviour
+{
+   [SerializeField] private Material Purple;
+   [SerializeField] private Material Blue;
+   [SerializeField] private Material Red;
+   
+   private void Start()
+   {
+      
+      switch (Random.Range(0, 3))
+      {
+         case 0:
+            RenderSettings.skybox = Purple;
+            //Debug.Log("Purple");
+            break;
+         case 1:
+            RenderSettings.skybox = Blue;         
+            //Debug.Log("Blue");
+            break;
+         case 2:
+            RenderSettings.skybox = Red;
+            //Debug.Log("Red");
+            break;
+      }
+   }
+}
